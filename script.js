7aa2256713928b0b545ab488167c4f7ec276f953
rajonahmed <ra1490449@gmail.com>
2021-02-17 02:58:11 +0600

    last bofore sleep

---
+++ script.js
@@ -0,0 +1,8 @@
+let days= new Date();
+console.log(days);
+
+const times= document.getElementById('times');
+ let newTime= document.createElement('h3');
+ newTime.innerText= days;
+ times.appendChild(newTime);
+ 
