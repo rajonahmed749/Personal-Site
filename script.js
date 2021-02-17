ce0338e673d393e2ead06abfc9eee14b174013c9
rajonahmed <ra1490449@gmail.com>
2021-02-17 22:36:10 +0600

    last upload

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-var myModal = new bootstrap.Modal(document.getElementById('myModal'), options);
-console.log(myModal);
+// const popup= document.getElementById('close-popup')
+
+// popup.addEventListener('keypress',function(event){
+//     if(event.key== 'Enter'){
+//         document.getElementById('close-popup').click()
+//     }
+// })
